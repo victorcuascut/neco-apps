59877429df17305ceac275d35160e00865add88d
UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>
2020-07-29 16:02:03 +0900

    ci: execute clean-dns.sh every morning.
    Signed-off-by: UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>

--- bin/clean-dns.sh
+++ bin/clean-dns.sh
@@ -2,7 +2,10 @@
 PROJECT=neco-test
 ZONE=gcp0
 GCLOUD_DNS="gcloud dns --project=$PROJECT record-sets"
+ECHO_TEST=""
 MAX_LINES=500
+
+# this script cannot handle mutiple value RRset (e.g. multiple address to same host name)
 
 ${GCLOUD_DNS} list --zone=$ZONE --filter="NOT TYPE=(SOA NS)" | tail -n +2 > zone.txt
 lines=$(cat zone.txt | wc -l)
@@ -11,9 +14,9 @@ if [ $lines -eq 0 ]; then
 fi
 loops=$(((lines - 1) / MAX_LINES + 1))
 for i in $(seq $loops); do
-  ${GCLOUD_DNS} transaction start --zone=$ZONE
+  ${ECHO_TEST} ${GCLOUD_DNS} transaction start --zone=$ZONE
   tail -n +$(((i-1)*MAX_LINES+1)) zone.txt | head -n ${MAX_LINES} | while read name type ttl data; do
-      ${GCLOUD_DNS} transaction remove "$data" --name=$name --type=$type --ttl=$ttl --zone=$ZONE
+      ${ECHO_TEST} ${GCLOUD_DNS} transaction remove "$data" --name=$name --type=$type --ttl=$ttl --zone=$ZONE
   done
-  ${GCLOUD_DNS} transaction execute --zone=$ZONE
+  ${ECHO_TEST} ${GCLOUD_DNS} transaction execute --zone=$ZONE
 done
