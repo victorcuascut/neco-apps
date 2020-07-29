3079a7ccdce734a37fe33e2813594ff3e9d0f138
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2020-07-29 05:56:58 +0000

    ci: clean DNS entries for test.
    Signed-off-by: morimoto-cybozu <kenji_morimoto@cybozu.co.jp>

--- bin/clean-dns.sh
+++ bin/clean-dns.sh
@@ -1,4 +1,19 @@
 #! /bin/bash -e
+PROJECT=neco-test
+ZONE=gcp0
+GCLOUD_DNS="gcloud dns --project=$PROJECT record-sets"
+MAX_LINES=500
 
-gcloud --project=neco-test dns record-sets export --zone-file-format --zone=gcp0 zone-gcp0
-gcloud --project=neco-test dns record-sets transaction start --zone=gcp0
+${GCLOUD_DNS} list --zone=$ZONE --filter="NOT TYPE=(SOA NS)" | tail -n +2 > zone.txt
+lines=$(cat zone.txt | wc -l)
+if [ $lines -eq 0 ]; then
+  exit
+fi
+loops=$(((lines - 1) / MAX_LINES + 1))
+for i in $(seq $loops); do
+  ${GCLOUD_DNS} transaction start --zone=$ZONE
+  tail -n +$(((i-1)*MAX_LINES+1)) zone.txt | head -n ${MAX_LINES} | while read name type ttl data; do
+      ${GCLOUD_DNS} transaction remove "$data" --name=$name --type=$type --ttl=$ttl --zone=$ZONE
+  done
+  ${GCLOUD_DNS} transaction execute --zone=$ZONE
+done
